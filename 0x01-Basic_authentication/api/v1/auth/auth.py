2ecb54cd72211ddd88818b9a128e9090b20e6f42
abeni-al7 <abenezeralebachew3@gmail.com>
2024-08-06 02:17:25 +0300

    Implement the logic to require authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python3
 """Authentication module"""
 from flask import request
-from typing import List, TypeVar
+from typing import List, TypeVar, Union
 
 
 class Auth:
     """Authentication class"""
-    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Returns False"""
-        return False
+    def require_auth(self, path: Union[str, None], excluded_paths: List[str]) -> bool:
+        """Determines if the path requires authentication"""
+        if path == None:
+            return True
+        if excluded_paths is None or excluded_paths == []:
+            return True
+        if not path.endswith('/'):
+            path += '/'
+        if path in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Returns None"""
