18aa1168aec40ec6d1503e921f58e5639ccf3ae7
abeni-al7 <abenezeralebachew3@gmail.com>
2024-08-06 04:39:13 +0300

    Implement authorization header detection

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python3
 """Basic auth implementation method"""
 from api.v1.auth.auth import Auth
+from typing import Union
 
 
 class BasicAuth(Auth):
     """Basic Auth implementation class"""
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header:
+                                            str) -> Union[
+                                                 str, None]:
+        """Returns base64 part of authorization header"""
+        if authorization_header is None:
+            return None
+        if not type(authorization_header) == str:
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        return authorization_header[6:]
