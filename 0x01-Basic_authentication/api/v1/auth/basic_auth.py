0b53814ea17fd666b3cd38e2559a1c2c39271523
abeni-al7 <abenezeralebachew3@gmail.com>
2024-08-06 04:57:43 +0300

    Implement base64 decoding feature

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -20,7 +20,6 @@ class BasicAuth(Auth):
             return None
         return authorization_header[6:]
 
-
     def decode_base64_authorization_header(self,
                                            base64_authorization_header:
                                            str) -> str:
