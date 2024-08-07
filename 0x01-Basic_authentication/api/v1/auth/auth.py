feddc824ff12ec2481ce2459ca7791e3852134a9
abeni-al7 <abenezeralebachew3@gmail.com>
2024-08-07 03:09:10 +0300

    Allow * in excluded paths list

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,14 +9,17 @@ class Auth:
     def require_auth(self, path: Union[str, None],
                      excluded_paths: List[str]) -> bool:
         """Determines if the path requires authentication"""
-        if path is None:
-            return True
-        if excluded_paths is None or excluded_paths == []:
-            return True
-        if not path.endswith('/'):
-            path += '/'
-        if path in excluded_paths:
-            return False
+        if path and excluded_paths:
+            for excluded in excluded_paths:
+                pattern = ''
+                if excluded[-1] == '*':
+                    pattern = f'{excluded[0:-1]}.*'
+                elif excluded[-1] == '/':
+                    pattern = f'{excluded[0:-1]}/*'
+                else:
+                    pattern = f'{excluded}/*'
+                if re.match(pattern, path):
+                    return False
         return True
 
     def authorization_header(self, request=None) -> str:
