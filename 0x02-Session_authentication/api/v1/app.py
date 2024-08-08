71a59b9c131cafa6bdffab47a03f611637fa098e
abeni-al7 <abenezeralebachew3@gmail.com>
2024-08-08 21:16:00 +0300

    Add login to excluded paths from auth

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -55,7 +55,7 @@ def before_request() -> str:
                                             '/api/v1/auth_session/login/']):
         return
     if auth.authorization_header(request) is None and \
-        auth.session_cookie(request) is None:
+       auth.session_cookie(request) is None:
         abort(401)
     if not auth.current_user(request):
         abort(403)
