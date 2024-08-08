b998a9fbbc525a7eadfcc8d3cd85fa463e23a180
abeni-al7 <abenezeralebachew3@gmail.com>
2024-08-08 23:03:42 +0300

    Implement session destroying feature

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -29,7 +29,8 @@ def login():
     return response
 
 
-@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/auth_session/logout',
+                 methods=['DELETE'], strict_slashes=False)
 def logout():
     """View for user logout"""
     from api.v1.app import auth
