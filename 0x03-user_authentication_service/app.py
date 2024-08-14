86f978e4eece14ed00ff70fabc4a384bf301c750
abeni-al7 <abenezeralebachew3@gmail.com>
2024-08-14 21:43:52 +0300

    Add a route for user registration

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,15 +1,28 @@
 #!/usr/bin/env python3
 """A module for a flask app"""
 from flask import Flask, jsonify, request, abort, make_response
+from auth import Auth
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index():
     """The index of the API"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def register_user():
+    """An endpoint for user registration"""
+    email = request.form["email"]
+    password = request.form["password"]
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"}), 201
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
