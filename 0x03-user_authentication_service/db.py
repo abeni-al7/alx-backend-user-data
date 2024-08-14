1873c4371cd5d6b9d97f90c5e2ee10a215bb1388
abeni-al7 <abenezeralebachew3@gmail.com>
2024-08-14 19:43:47 +0300

    Solve

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -41,9 +41,4 @@ class DB:
 
     def find_user_by(self, **kwargs: dict) -> User:
         """Find a user from the database"""
-        if not kwargs:
-            raise InvalidRequestError
-        user = self._session.query(User).filter_by(**kwargs).first()
-        if not user:
-            raise NoResultFound
-        return user
+        return self._session.query(User).filter_by(**kwargs).one()
