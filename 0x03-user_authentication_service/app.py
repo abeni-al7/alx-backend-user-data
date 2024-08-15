48794fe9f5eb97b6e7bc11ca1c592f52663f4a8c
abeni-al7 <abenezeralebachew3@gmail.com>
2024-08-15 23:58:41 +0300

    Create a login endpoint

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -25,5 +25,19 @@ def register_user():
         return jsonify({"message": "email already registered"})
 
 
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """An endpoint for user login"""
+    email = request.form["email"]
+    password = request.form["password"]
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        if session_id:
+            response = jsonify({"email": email, "message": "logged in"})
+            response.set_cookie("session_id", session_id)
+            return response
+    abort(401)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
