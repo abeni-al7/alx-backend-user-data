a178627d65d0fe20f058bbee1e64436e01d4b08b
abeni-al7 <abenezeralebachew3@gmail.com>
2024-08-16 00:07:22 +0300

    Log in users based on their session_id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -51,8 +51,10 @@ class Auth:
         except Exception:
             return None
 
-    def get_user_from_session_id(self, session_id: str) -> User | None:
+    def get_user_from_session_id(self, session_id: str) -> User:
         """Returns a logged in user"""
+        if session_id is None:
+            return None
         try:
             user = self._db.find_user_by(session_id=session_id)
             return user
